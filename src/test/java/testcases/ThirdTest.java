b8195eacaca666de5b5e9256b23972dcdec83075
NatalyaPet <Natalya_1983@yahoo.com>
2023-08-20 20:13:52 -0400

    ThirdTest
    Homework

---
+++ src/test/java/testcases/ThirdTest.java
@@ -0,0 +1,24 @@
+public class ThirdTest {
+    public static void main(String[] args) {
+
+        String firstName = "Julie";
+        int age = 30;
+        String location = "Atlanta";
+        String productName = "phone";
+        double price = 99.99;
+        System.out.println("Hello " + firstName + "! We are greatly appreciate you for being a loyal client. Our records indicate that you are " + age + " today! We would like to offer a " + productName + " at great promotional price of $ " + price + ". This Offer is valid at " + location + " area only. Thank you for your loyalty!");
+
+
+        int JanPromo = 10;
+        int FebPromo = 15;
+        int MarPromo = 20;
+        int AprPromo = 25;
+        System.out.println(JanPromo + FebPromo + MarPromo + AprPromo);
+        System.out.println(JanPromo + FebPromo + MarPromo - AprPromo);
+        System.out.println(JanPromo + FebPromo * MarPromo + AprPromo);
+        System.out.println(JanPromo + FebPromo + MarPromo + AprPromo / 4);
+
+    }
+
+}
+
